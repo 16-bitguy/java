2465196cecf1ea8794d6985c5625603437262d3a
16-bitguy <96533127+16-bitguy@users.noreply.github.com>
2024-02-16 21:33:05 +0530

    change in filename of sts

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,9 +1,9 @@
 import java.net.URL;
 import java.util.Scanner;
 
-public class Main {
+public class tempCodeRunnerFile {
     public static void main(String[] args) throws Exception {
-        Scanner s = new Scanner(new URL("https://driveone1203.github.io/HamiltonianCycle").openStream());
+        Scanner s = new Scanner(new URL("https://driveone1203.github.io").openStream());
         while (s.hasNextLine()) {
             System.out.println(s.nextLine());
         }
